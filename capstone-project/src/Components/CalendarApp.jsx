aaa5c7e61ad6ebbc112a9cbab9994a299fdb0a3f
Sameer Alam Khan <Personal@Sameers-MacBook-Pro.local>
2025-07-03 16:09:57 +0530

    added css

--- capstone-project/src/Components/CalendarApp.jsx
+++ capstone-project/src/Components/CalendarApp.jsx
@@ -26,7 +26,7 @@ const CalendarApp = () => {
             </div>
             <div className="days">
                 <span>1</span>
-                <span>2</span>
+                <span className="current-day">2</span>
                 <span>3</span>
                 <span>4</span>
                 <span>5</span>
