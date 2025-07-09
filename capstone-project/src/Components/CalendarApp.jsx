e89e9bbf6dec028dc42df67c8ba0b33aadac49a5
Sameer Alam Khan <Personal@Sameers-MacBook-Pro.local>
2025-07-09 21:02:08 +0530

    added dynamic event messages with the ability to edit said events

--- capstone-project/src/Components/CalendarApp.jsx
+++ capstone-project/src/Components/CalendarApp.jsx
@@ -15,18 +15,18 @@ const CalendarApp = () => {
     const [events,setEvents] = useState([])
     const [eventTime,setEventTime] = useState({hours:'00',minutes:'00'})
     const [eventText,setEventText] = useState('')
-    
+    const[editingEvent,setEditingEvent] = useState(null)
     const daysInMonth = new Date(currentYear,currentMonth+1,0).getDate()
 
     const firstDayOfMonth = new Date(currentYear,currentMonth,1).getDay()
 
     const prevMonth=()=>{
-        setCurrentMonth((prevMonth)=>(currentMonth===0?11:prevMonth-1))
+        setCurrentMonth((prevMonth)=>(currentMonth===0?11:currentMonth-1))
         setCurrentYear(prevYear=> currentMonth===0?prevYear-1:prevYear)
     }
 
     const nextMonth=()=>{
-        setCurrentMonth((nextMonth)=>(currentMonth===11?0:nextMonth+1))
+        setCurrentMonth((nextMonth)=>(currentMonth===11?0:currentMonth+1))
         setCurrentYear(nextYear=> currentMonth===11?nextYear+1:nextYear)
     }
 
@@ -39,6 +39,7 @@ const CalendarApp = () => {
             setShowEventPopup(true)
             setEventText("")
             setEventTime({hours:'00',minutes:'00'})
+            setEditingEvent(null)
         }
     }
 
@@ -52,16 +53,42 @@ const CalendarApp = () => {
 
     const handleEventSubmit = ()=>{
         const newEvent = {
+            id : editingEvent?editingEvent.id : Date.now(),
             date: selectedDate,
             time: `${eventTime.hours.padStart(2,'0')}:${eventTime.minutes.padStart(2,'0')}`,
             text: eventText
         }
-        setEvents([...events,newEvent])
+
+        let updatedEvents = [...events]
+        if (editingEvent){
+            updatedEvents = updatedEvents.map((event)=>
+                 event.id === editingEvent.id?newEvent:event,)
+        }
+        else{
+            updatedEvents.push(newEvent)
+        }
+
+        updatedEvents.sort((a,b)=> new Date(a.date) - new Date(b.date))
+
+        setEvents([updatedEvents])
         setEventTime({hours:'00',minutes:'00'})
         setEventText("")
         setShowEventPopup(false)
+        setEditingEvent(null)
 
     }   
+
+    const handleEditEvent=(event)=>{
+        setSelectedDate(new Date(event.date))
+        setEventTime({
+            hours: event.time.split(":")[0],
+            minutes : event.time.split(":")[1],
+        })
+        setEventText(event.text)
+        setEditingEvent(event)
+        setShowEventPopup(true)
+
+    }
 
     const CloseEventPopup=()=>{
         setShowEventPopup(false)
@@ -84,15 +111,18 @@ const CalendarApp = () => {
                 </div>
             </div>
             <div className="weekdays">
-                {daysOfWeek.map((day)=><span key={day}> {day}</span>)}
+                {daysOfWeek.map((day)=>(
+                    <span key={day}> {day}</span>))}
             </div>
             <div className="days">
                 {[...Array(firstDayOfMonth).keys()].map((_,index)=> (
-                <span key={`empty-${index}`}/>))}
+                <span/>))}
 
-
-            {[...Array(daysInMonth).keys()].map((day)=>
-            <span key={day+1} className={day+1===currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear===currentDate.getFullYear()?"current-day":""} onClick={()=> handleDayClick(day+1)}>{day+1}</span>)}
+                {[...Array(daysInMonth).keys()].map((day)=>
+                <span 
+                key={day+1} 
+                className={day+1===currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear===currentDate.getFullYear()?"current-day":""} onClick={()=> handleDayClick(day+1)}>{day+1}
+                </span>)}
             
             </div>
         </div>
@@ -100,30 +130,52 @@ const CalendarApp = () => {
             {showEventPopup &&(<div className="event-popup">
                 <div className="time-input">
                     <div className="event-popup-time">Time</div>
-                    <input type="number" name="hours" min={0} max={24} className="hours" value ={eventTime.hours} onChange={(e)=>{setEventTime({...eventTime,hours:e.target.value})}}/>
-                    <input type="number" name="minutes" min={0} max={60} className="minutes"/>
-
-
+                    <input 
+                        type="number" 
+                        name="hours" 
+                        min={0} 
+                        max={24} 
+                        className="hours" 
+                        value ={eventTime.hours} 
+                        onChange={(e)=>{setEventTime({...eventTime,hours:e.target.value})}}/>
+                    <input 
+                        type="number" 
+                        name="minutes" 
+                        min={0} 
+                        max={60} 
+                        className="minutes"
+                        value ={eventTime.minutes} 
+                        onChange={(e)=>{setEventTime({...eventTime,minutes:e.target.value})}}/>
                 </div>
-                <textarea placeholder="Enter Event Text (Maximum 60 Characters) "></textarea>
-                <button className="event-popup-btn">Add Event</button>
+                <textarea placeholder="Enter Event Text (Maximum 60 Characters)" 
+                value={eventText}
+                onChange={(e)=>{
+                    if (e.target.value.length <= 60){
+                        setEventText(e.target.value)
+                    }
+                }}
+                ></textarea>
+                <button className="event-popup-btn" onClick={handleEventSubmit}>{editingEvent?"Update Event" : "Add Event"}</button>
                 <button className="close-event-popup" onClick={CloseEventPopup}>
                     <i className="bx bx-x"><svg  xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill={"currentColor"} viewBox="0 0 24 24">{/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}<path d="m7.76 14.83-2.83 2.83 1.41 1.41 2.83-2.83 2.12-2.12.71-.71.71.71 1.41 1.42 3.54 3.53 1.41-1.41-3.53-3.54-1.42-1.41-.71-.71 5.66-5.66-1.41-1.41L12 10.59 6.34 4.93 4.93 6.34 10.59 12l-.71.71z"></path></svg></i>
                 </button>
             </div>)}
-            <div className="event">
+            {events.map((event,index)=>(
+            <div className="event" key={index}>
                 <div className="event-date-wrapper">
-                    <div className="event-date">July 1 2025</div>
-                    <div className="event-time">21:00</div>
-
+                    <div className="event-date">{`${monthsOfYear[event.date.getMonth()]} ${event.date.getDate()} ${event.date.getFullYear()}`}</div>
+                    <div className="event-time">{event.time}</div>
                 </div>
-                <div className="event-text">capstone Project Work</div>
+                
+                <div className="event-text">{event.text}</div>
                 <div className="event-buttons">
-                    <i className="bx bxs-edit-alt"><svg  xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill={"currentColor"} viewBox="0 0 24 24">{/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}<path d="m17.71 7.29-3-3a.996.996 0 0 0-1.41 0l-11.01 11A1 1 0 0 0 2 16v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l11-11a.996.996 0 0 0 0-1.41ZM5.59 18H4v-1.59l7.5-7.5 1.59 1.59zm8.91-8.91L12.91 7.5 14 6.41 15.59 8zM11 18h11v2H11z"></path></svg></i>
+                    <i className="bx bxs-edit-alt" onClick={()=> handleEditEvent(event)}><svg  xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill={"currentColor"} viewBox="0 0 24 24">{/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}<path d="m17.71 7.29-3-3a.996.996 0 0 0-1.41 0l-11.01 11A1 1 0 0 0 2 16v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l11-11a.996.996 0 0 0 0-1.41ZM5.59 18H4v-1.59l7.5-7.5 1.59 1.59zm8.91-8.91L12.91 7.5 14 6.41 15.59 8zM11 18h11v2H11z"></path></svg></i>
                     <i className="bx bxs-message-alt-x"><svg  xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill={"currentColor"} viewBox="0 0 24 24">{/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}<path d="m7.76 14.83-2.83 2.83 1.41 1.41 2.83-2.83 2.12-2.12.71-.71.71.71 1.41 1.42 3.54 3.53 1.41-1.41-3.53-3.54-1.42-1.41-.71-.71 5.66-5.66-1.41-1.41L12 10.59 6.34 4.93 4.93 6.34 10.59 12l-.71.71z"></path></svg></i>
 
                 </div>
             </div>
+            ))}
+            
         </div>
     </div>
   )
